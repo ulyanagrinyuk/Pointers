4cb0f7ec503dff9487ed0dc33ee39f38c5099235
Uliana29 <Uliana29@DESKTOP-I69RM28>
2023-04-22 15:09:58 +0300

    Encoding

---
+++ DynamicMemory/Source.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "¬ведите размер массива:"; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+
+
+	int value;
+	cout << "¬ведите добавл€емое значение:"; cin >> value;
+	//1
+	int* buffer = new int[n + 1];
+	//2
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3
+	delete[] arr;
+	//4
+	arr = buffer;
+	buffer = nullptr;
+	cout << typeid(nullptr).name() << endl;
+	//5
+	arr[n] = value;
+	//6
+	n++;
+	//7
+	Print(arr, n);
+
+
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			*(arr + i) = rand() % 100;
+		}
+	}
+void Print(int arr[], const int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
---
+++ Reference/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int& ra = a;
+	ra += 3;
+	cout << a << endl;
+}
