8bca1f6bec358a1cb9f8899998deb8733d196ef9
Uliana29 <Uliana29@DESKTOP-I69RM28>
2023-04-29 17:59:32 +0300

    Encoding

--- DynamicMemory/Source.cpp
+++ DynamicMemory/Source.cpp
@@ -1,22 +1,129 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 void FillRand(int arr[], const int n);
-void Print(int arr[], const int n);
+void FillRand(int** arr, const int rows, const int cols);
 
+void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
+
+int* push_back(int* arr, int& n, int value);
+int* push_front(int* arr, int& n, int value);
+
+int* insert(int* arr, int& n, int value, int index);
+
+int* pop_back(int* arr, int& n);
+int* pop_front(int* arr, int& n);
+
+//#define DINAMIC_MEMORY_1
+#define DINAMIC_MEMORY_2
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DINAMIC_MEMORY_1
+
+
+
 	int n;
 	cout << "¬ведите размер массива:"; cin >> n;
 	int* arr = new int[n];
 	FillRand(arr, n);
 	Print(arr, n);
 
-
 	int value;
 	cout << "¬ведите добавл€емое значение:"; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+
+	cout << "¬ведите добавл€емое значение:"; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	int index;
+	cout << "¬ведите индекс добавл€емое значение:"; cin >> index;
+	cout << "¬ведите добавл€емое значение:"; cin >> value;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
+#endif // DINAMIC_MEMORY_1
+
+	int rows, cols;
+	cout << "¬ведите кол-во строк:"; cin >> rows;
+	cout << "¬ведите кол-во элементы строк:"; cin >> cols;
+
+
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+
+	//1
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//2
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int* push_back(int* arr, int& n, int value)
+{
 	//1
 	int* buffer = new int[n + 1];
 	//2
@@ -35,24 +142,61 @@ void main()
 	//6
 	n++;
 	//7
-	Print(arr, n);
-
-
+	return arr;
+}
+int* push_front(int* arr, int& n, int value)
+{
+	int* buffer = new int[n + 1] {};
+	buffer[0] = value;
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
 	delete[] arr;
+	arr = buffer;
+	n++;
+	return arr;
 }
 
-void FillRand(int arr[], const int n)
+int* insert(int* arr, int& n, int value, int index)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < index; i++)
 	{
-		for (int i = 0; i < n; i++)
-		{
-			*(arr + i) = rand() % 100;
-		}
+		buffer[i] = arr[i];
 	}
-void Print(int arr[], const int n)
+	for (int i = index; i < n; i++)
 	{
-		for (int i = 0; i < n; i++)
-		{
-			cout << arr[i] << "\t";
-		}
-		cout << endl;
+		buffer[i + 1] = arr[i];
 	}
+	delete[] arr;
+	arr = buffer;
+	arr[index] = value;
+	n++;
+	return arr;
+}
+
+int* pop_back(int* arr, int& n)
+{
+	/*int* buffer = new int[n - 1];
+	for (int i = 0; i < n - 1; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	n--;
+	return arr;*/
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++) buffer[i] = arr[i];
+	delete[]arr;
+	return buffer;
+}
+int* pop_front(int* arr, int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[] arr;
+	return buffer;
+}
+
